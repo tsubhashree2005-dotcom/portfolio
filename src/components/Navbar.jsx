2407562668f19360f9cc6fdb31f33827756d3dfd
T SUBHASHREE <tsubhashree@Ts-MacBook-Air.local>
2026-07-13 17:11:56 +0530

    Fixed mobile responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,35 @@
+
+import { useState } from "react";
 import "../styles/Navbar.css";
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
 
       <h2 className="logo">T SUBHA SHREE</h2>
 
-      <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#contact">Contact</a></li>
+      <div
+        className="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        MENU
+      </div>
+
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+
+        <li><a href="#home" onClick={() => setMenuOpen(false)}>Home</a></li>
+
+        <li><a href="#about" onClick={() => setMenuOpen(false)}>About</a></li>
+
+        <li><a href="#projects" onClick={() => setMenuOpen(false)}>Projects</a></li>
+
+        <li><a href="#experience" onClick={() => setMenuOpen(false)}>Experience</a></li>
+
+        <li><a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a></li>
+
       </ul>
 
     </nav>
